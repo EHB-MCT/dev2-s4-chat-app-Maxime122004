28732b9ee59784dd4d9b9ec58b6a935ce654d3db
Maxime Bastien <maxime.bastien@student.ehb.be>
2023-03-15 15:38:55 +0100

    message

--- js/index.js
+++ js/index.js
@@ -3,14 +3,44 @@
 const chat = {
     author: "yourName",
     init() {
+        this.fetchMessages();
     },
     sendMessage() {
     },
     fetchMessages() {
+        fetch('https://dev2chat.onrender.com/messages')
+            .then(function (response) {
+                return response.json();
+            }).then(function (messages) {
+                console.log(messages);
+
+               chat.renderMessage();
+            });
+
     },
-    renderMessage(message) {
+    renderMessage(messages) {
+        messages.forEach(function (message) {
+
+            const htmlString = `   
+                <div class="messageItem">
+                
+                    <div class="header">
+                        <span class="author">${message.author}</span>
+                        <span class="time">${message.created_at}</span>
+                    </div>
+                    <p>
+                        ${message.message}
+                    </p>
+                </div>`;
+
+            document.querySelector('#messageContainer').append('beforeend', htmlString);
+        });
     }
 
-}
+};
 
 chat.init();
+
+window.setInterval(function (){
+    chat.fetchMessages();
+}, 5000);
